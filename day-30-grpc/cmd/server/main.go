d92d7bd07a95113967f8a6ee93358f6eace58c8c
pruthviraj7714 <marganepruthviraj@gmail.com>
2026-07-16 22:59:26 +0530

    feat: implement user service with gRPC endpoints

--- day-30-grpc/cmd/server/main.go
+++ day-30-grpc/cmd/server/main.go
@@ -1,12 +1,66 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"grpc/internal/config"
 	"grpc/internal/database"
+	"grpc/internal/models"
+	"grpc/pb"
+	"log"
+	"net"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"gorm.io/gorm"
 )
+
+type userserver struct {
+	pb.UnimplementedUserServiceServer
+	DB *gorm.DB
+}
+
+func (s *userserver) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+
+	user := models.User{
+		Name:     req.Name,
+		Email:    req.Email,
+		Password: req.Password,
+	}
+
+	err := s.DB.Create(&user).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CreateUserResponse{
+		User: &pb.User{
+			Id:       user.ID,
+			Name:     user.Name,
+			Email:    user.Email,
+			Password: user.Password,
+		},
+	}, nil
+}
+
+func (s *userserver) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.GetUserResponse, error) {
+	var user models.User
+
+	err := s.DB.First(&user, req.Id).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.GetUserResponse{
+		User: &pb.User{
+			Id:       user.ID,
+			Name:     user.Name,
+			Email:    user.Email,
+			Password: user.Password,
+		},
+	}, nil
+}
 
 func main() {
 	r := gin.Default()
@@ -15,7 +69,24 @@ func main() {
 
 	db := database.Connect(cfg.DBURL)
 
-	fmt.Print(db)
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	userService := &userserver{
+		DB: db,
+	}
+
+	pb.RegisterUserServiceServer(grpcServer, userService)
+
+	log.Printf("gRPC server starting on port :50051...")
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 
 	r.Run(cfg.PORT)
 }
--- day-30-grpc/go.mod
+++ day-30-grpc/go.mod
@@ -35,12 +35,14 @@ require (
 	github.com/ugorji/go/codec v1.3.1 // indirect
 	go.mongodb.org/mongo-driver/v2 v2.5.0 // indirect
 	golang.org/x/arch v0.22.0 // indirect
-	golang.org/x/crypto v0.48.0 // indirect
-	golang.org/x/net v0.51.0 // indirect
-	golang.org/x/sync v0.19.0 // indirect
-	golang.org/x/sys v0.41.0 // indirect
-	golang.org/x/text v0.34.0 // indirect
-	google.golang.org/protobuf v1.36.10 // indirect
+	golang.org/x/crypto v0.50.0 // indirect
+	golang.org/x/net v0.53.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260414002931-afd174a4e478 // indirect
+	google.golang.org/grpc v1.82.1 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 	gorm.io/driver/postgres v1.6.0 // indirect
 	gorm.io/gorm v1.25.10 // indirect
 )
